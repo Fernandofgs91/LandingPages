7f5c991ebf71dc83bbb5006ec5cf26ffd6b06046
Fernandofgs91 <fernando.guimaraes@estudante.iftm.edu.br>
2026-06-15 20:27:23 -0300

    Correção

--- src/componentes/Contato/Contato.jsx
+++ src/componentes/Contato/Contato.jsx
@@ -1,6 +1,6 @@
 import React, { useRef } from "react";
 import emailjs from "@emailjs/browser";
-import styles from "./contato.module.css";
+import styles from "./Contato.module.css";
 
 const Contato = () => {
   const form = useRef();
