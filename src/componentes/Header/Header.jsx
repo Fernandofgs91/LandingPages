0b6e860430e3a92a7484b46d940de9f2e51c5701
Fernandofgs91 <fernando.guimaraes@estudante.iftm.edu.br>
2026-06-17 14:08:43 -0300

    animacao

--- src/componentes/Header/Header.jsx
+++ src/componentes/Header/Header.jsx
@@ -2,42 +2,32 @@ import React from "react";
 import CTA from "../CTA/CTA";
 import styles from "./header.module.css";
 
-// import plantaImg from "../../assets/header/imagemprojetoheader.png";
-
 const Header = () => {
   return (
-    <section
-      id="header"  className={styles.hero} >
-     
+    <section id="header" className={styles.hero}>
       <div className={styles.overlay}></div>
+
       <div className={styles.containerHeader}>
         <div className={styles.content}>
-    <h1 className={styles.titleHeader}>
-  COMPROMETIMENTO
-  <br />
-  <strong>E RESPONSABILIDADE</strong>
-  <br />
-  COM OS SEUS <span className={styles.highlight}>SONHOS</span>
-</h1>
+          <h1 className={styles.titleHeader}>
+            COMPROMETIMENTO
+            <br />
+            <strong>E RESPONSABILIDADE</strong>
+            <br />
+            COM OS SEUS <span className={styles.highlight}>SONHOS</span>
+          </h1>
 
           <p className={styles.descriptionHeader}>
             Elaboração de projetos, execução e administração de obra,
             regularização e consultoria, conforme os padrões legais
             normativos dos órgãos municipais.
           </p>
-             <CTA />
-         
+
+          <div className={styles.ctaWrapper}>
+            <CTA />
+          </div>
         </div>
- {/* Coluna da Direita: Imagem */}
-        {/* <div className={styles.compromissoImageWrapper}>
-          <img 
-            src={plantaImg} 
-            alt="Planta arquitetônica de projeto de engenharia" 
-            className={styles.compromissoImg}
-          />
-        </div> */}
       </div>
-      
     </section>
   );
 };
